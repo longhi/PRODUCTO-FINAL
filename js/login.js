b1ecf8b168c3b517dd4548dc72d4af445098908f
NelidaSh <nelida.quispetacuri@gmail.com>
2018-01-09 10:31:42 -0500

    agregando funcion de inicio sesion

--- js/login.js
+++ js/login.js
@@ -1,10 +1,54 @@
 $(document).ready(function() {
   $('.modal').modal();
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAjNJLqG0zs1iy-VHo1NueO4DRQzEaDFdE",
+    authDomain: "producto-final-8583e.firebaseapp.com",
+    databaseURL: "https://producto-final-8583e.firebaseio.com",
+    projectId: "producto-final-8583e",
+    storageBucket: "producto-final-8583e.appspot.com",
+    messagingSenderId: "452395891662"
+  };
 
-  var $email = $('#email1');
-  var $password = $('#password1');
+  firebase.initializeApp(config);
 
-  $('.register').on('click', function(event) {
+  /*$('.register').on('click', function(event) {
+    var email = $('#email1').val();
+    var password = $('#password1').val();
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  });
+
+  $('.login').on('click', function(event) {
+    var email2 = $('#email').val();
+    var password2 = $('#password').val();
+    firebase.auth().signInWithEmailAndPassword(email2, password2).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);// ...
+    });
+    //
+  });*/
+
+  $('#inigoogle').on('click', function() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      window.location.href = 'home.html';
+      // var token = result.credential.accessToken;
+      // The signed-in user info.
+      // var user = result.user;
+    });
+  });
+  // var $email = $('#email1');
+  // var $password = $('#password1');
+
+  /*$('.register').on('click', function(event) {
     event.preventDefault();
     localStorage.email = $email.val();
     localStorage.password = $password.val();
@@ -39,5 +83,5 @@ $(document).ready(function() {
     if (validateEmail && validatePassword) {
       window.location.href = 'home.html';
     }
-  });
+  });*/
 });
