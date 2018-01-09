90e37092e461f330b0f2bca5d0c0dc2b35002f53
NelidaSh <nelida.quispetacuri@gmail.com>
2018-01-09 00:44:28 -0500

    añadiendo funcion envio de texto

--- js/home.js
+++ js/home.js
@@ -1,4 +1,24 @@
-// Initialize collapse button
- $(".button-collapse").sideNav();
- // Initialize collapsible (uncomment the line below if you use the dropdown variation)
- //$('.collapsible').collapsible();
+$(document).ready(function () {
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+
+  var $tweetArea = $('.new-text');
+  var $tweetBtn = $('#send');
+  var $row = $('#content');
+
+
+  $tweetBtn.on('click', function(){
+    // event.preventDefault();
+    if ($tweetArea.val()) {
+      $row.prepend('<div class="new-item"><div class="row"><div class="col s10" id="new-container"></div></div></div>');
+      var $text = $('#new-container');
+      var $parrafo = $('<p/>', { 'class': 'paragraph' });
+      $parrafo.text($tweetArea.val());
+      $text.prepend($parrafo);
+      $tweetArea.val('');
+      $tweetArea.focus();
+    }
+  });
+});
